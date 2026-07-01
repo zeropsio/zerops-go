2ffcf823bc5f72317c01ca6f49a0e34510c103a4
Michal Saloň <michal.salon93+vshosting@gmail.com>
2026-07-01 19:15:02 +0200

    Specifier fiexes

--- dto/output/esContainer.go
+++ dto/output/esContainer.go
@@ -13,17 +13,17 @@ import (
 var _ strconv.NumError
 
 type EsContainer struct {
-	Id                      uuid.ContainerId          `json:"id"`
-	ClientId                uuid.ClientId             `json:"clientId"`
-	ProjectId               uuid.ProjectId            `json:"projectId"`
-	InstanceId              uuid.InstanceId           `json:"instanceId"`
-	ServiceStackId          uuid.ServiceStackId       `json:"serviceStackId"`
-	ServiceId               uuid.ServiceId            `json:"serviceId"`
-	Status                  *enum.ContainerStatusEnum `json:"status"`
-	Number                  types.Int                 `json:"number"`
-	Name                    types.StringNull          `json:"name"`
-	Hostname                types.StringNull          `json:"hostname"`
-	CurrentHardwareResource HardwareResource          `json:"currentHardwareResource"`
-	Created                 types.DateTime            `json:"created"`
-	LastUpdate              types.DateTime            `json:"lastUpdate"`
+	Id                      uuid.ContainerId         `json:"id"`
+	ClientId                uuid.ClientId            `json:"clientId"`
+	ProjectId               uuid.ProjectId           `json:"projectId"`
+	InstanceId              uuid.InstanceId          `json:"instanceId"`
+	ServiceStackId          uuid.ServiceStackId      `json:"serviceStackId"`
+	ServiceId               uuid.ServiceId           `json:"serviceId"`
+	Status                  enum.ContainerStatusEnum `json:"status"`
+	Number                  types.Int                `json:"number"`
+	Name                    types.StringNull         `json:"name"`
+	Hostname                types.StringNull         `json:"hostname"`
+	CurrentHardwareResource HardwareResource         `json:"currentHardwareResource"`
+	Created                 types.DateTime           `json:"created"`
+	LastUpdate              types.DateTime           `json:"lastUpdate"`
 }
